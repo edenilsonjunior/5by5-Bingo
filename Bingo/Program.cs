c05897cc965bcfaef9366268f41ff24d746b5ab5
Edenilson <edenilsonju10@gmail.com>
2024-04-26 00:33:20 -0300

    impl functions to check bingo: vertically, horizontally and totally

--- Bingo/Program.cs
+++ Bingo/Program.cs
@@ -1,39 +1,4 @@
-﻿// Amount of players
-int playerAmount = 1;
-
-// Represents how many cards there are
-int cardsSize = playerAmount * 2;
-
-// Represents the index of where each card begins and ends
-int count;
-
-/*
- * Array that represents players names
- * Array size = amount of players
- * Each index of array = represents the name of player
- */
-string[] playersNames = new string[playerAmount];
-
-
-/*
- * Array that represents players cards
- * Array size = amount of players
- * Each index of array = represents how many cards each player has
- */
-int[] playersCards = new int[playerAmount];
-
-
-/* Array that represents players points
- * Array size = amount of players
- * Each index of array = represens how many points each player has
- */
-int[] playersPoints = new int[playerAmount];
-
-
-/*
- * Each player can have multiple cards, the control is made by var count
- */
-int[][,] cards = new int[cardsSize][,];
+﻿
 
 
 /*-----Functions-----*/
@@ -78,9 +43,9 @@ bool IsRepeated(int[,] matrix, int number)
 {
     bool repeated = false;
 
-    for(int line = 0; line < 5; line++)
+    for (int line = 0; line < 5; line++)
     {
-        for(int column = 0; column < 5; column++)
+        for (int column = 0; column < 5; column++)
         {
             if (matrix[line, column] == number)
             {
@@ -91,9 +56,16 @@ bool IsRepeated(int[,] matrix, int number)
     return repeated;
 }
 
-
-
-
+/*
+ * Sort and matrix for every card([][]) in the array
+ */
+void PopulateCards(int[][,] cards, int cardsSize)
+{
+    for (int i = 0; i < cardsSize; i++)
+    {
+        cards[i] = SortMatrix();
+    }
+}
 
 /*
  * Prints que matrix in the Console
@@ -105,11 +77,138 @@ void PrintMatrix(int[,] matrix, int lines, int columns, string title)
     {
         for (int c = 0; c < columns; c++)
         {
-            Console.Write($"{matrix[l, c]} ");
+            Console.Write($"{matrix[l, c]:00} ");
         }
         Console.WriteLine();
     }
 }
 
-cards[0] = SortMatrix();
-PrintMatrix(cards[0], 5, 5, "Matrix");
+bool IsBingoVertically(int[,] cards)
+{
+    bool bingo = false;
+
+    for (int l = 0; l < 5; l++)
+    {
+        int count = 0;
+        for (int c = 0; c < 5; c++)
+        {
+            if (cards[c, l] < 0)
+                count++;
+        }
+
+        if (count == 5)
+        {
+            bingo = true;
+            break;
+        }
+    }
+
+    return bingo;
+}
+
+
+
+
+bool IsBingoHorizontally(int[,] cards)
+{
+    bool bingo = false;
+
+    for (int l = 0; l < 5; l++)
+    {
+        int count = 0;
+        for (int c = 0; c < 5; c++)
+        {
+            if (cards[l, c] < 0)
+                count++;
+        }
+        if (count == 5)
+        {
+            bingo = true;
+            break;
+        }
+    }
+
+    return bingo;
+}
+
+bool IsTotallyBingo(int[,] cards)
+{
+    bool bingo = false;
+    int count = 0;
+
+    for (int l = 0; l < 5; l++)
+    {
+        for (int c = 0; c < 5; c++)
+        {
+            if (cards[c, l] < 0)
+                count++;
+        }
+    }
+    if (count == 25)
+        bingo = true;
+
+    return bingo;
+}
+
+
+// Amount of players
+const int playerAmount = 3;
+
+/*
+ * Array that represents players names
+ * Array size = amount of players
+ * Each index of array = represents the name of player
+ */
+string[] playersNames = new string[playerAmount];
+
+
+/*
+ * Array that represents players cards
+ * Array size = amount of players
+ * Each index of array = represents how many cards each player has
+ */
+int[] playersCards = new int[playerAmount] { 1, 2, 3 };
+
+
+/* Array that represents players points
+ * Array size = amount of players
+ * Each index of array = represens how many points each player has
+ */
+int[] playersPoints = new int[playerAmount];
+
+
+// Represents how many cards there are
+int cardsSize = 0;
+
+for (int i = 0; i < playersCards.Length; i++)
+{
+    cardsSize += playersCards[i];
+}
+
+
+/*
+ * Each player can have multiple cards, the control is made by var count
+ */
+int[][,] cards = new int[cardsSize][,];
+
+
+/*
+PopulateCards(cards, cardsSize);
+
+for (int i = 0; i < cardsSize; i++)
+{
+    PrintMatrix(cards[0], 5, 5, $"Matrix {i + 1}");
+    Console.WriteLine("\n");
+}*/
+
+
+int[,] bingo = { { -1, -2, -3, -4, -5 },
+                 { -1, -2, -3, -4, -5 },
+                 { -1, -2, -3, -4, -5 },
+                 { -1, -2, -3, -4, -5 },
+                 { -1, -2, -3, -4, -5 }
+               };
+
+Console.WriteLine($"bingo horizontal: {IsBingoHorizontally(bingo)}");
+Console.WriteLine($"bingo vertical: {IsBingoVertically(bingo)}");
+Console.WriteLine($"bingo total: {IsTotallyBingo(bingo)}");
